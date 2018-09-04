09670bb3645b652666e661ad90afba2cc66160a1
lixiang <214372150@qq.com>
2018-09-04 11:24:44 +0800

    test

--- main.ts
+++ main.ts
@@ -101,7 +101,7 @@ namespace CooCoo {
      * 设置蜂鸣器
      */
     //% weight=10
-    //% blockId="coocoo_buzz" block="播放音符二进制 %tone| 节拍 %beat"
+    //% blockId="coocoo_buzz" block="播放音符 %tone| 节拍 %beat"
     export function buzz(tone: ToneHzTable, beat: BeatList): void {
 
         let buf = pins.createBuffer(4);
