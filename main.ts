88098e0a73212ad3e6f2cbd0c75c1aa69a65b3f2
lixiang <214372150@qq.com>
2018-09-05 09:44:51 +0800

    测试音乐模块

--- main.ts
+++ main.ts
@@ -23,16 +23,27 @@ enum ToneHzTable {
 }
 
 enum BeatList {
-    //% block="八分之一"
-    EIGHTH = 50,
-    //% block="四分之一"
-    QUARTER = 100,
-    //% block="二分之一"
-    HALF = 200,
-    //% block="整拍"
-    FULL = 400,
-    //% block="双拍"
-    DOUBLE = 800
+    //% block="1"
+    WHOLE = BeatFraction.Whole,
+    //% block="1/2"
+    HALF = BeatFraction.Half,
+    //% block="1/4"
+    QUARTER = BeatFraction.Quarter,
+    //% block="1/8"
+    EIGHTH = BeatFraction.Eighth,
+    //% block="1/16"
+    SIXTEEN = BeatFraction.SixTeenth,
+    //% block="2"
+    DOUBLE = BeatFraction.Double,
+    //% block="4"
+    BREVE = BeatFraction.Breve 
+}
+
+enum MusicTable {
+    //% block="生日快乐"
+    birthday = Melodies.Birthday,
+    //% block="婚礼进行曲"
+    wedding = Melodies.Wedding
 }
 
 enum Patrol{
@@ -46,7 +57,118 @@ enum Patrol{
     black_black = 4
 }
 
-//% weight=70 icon="\uf0e7" color=#1B80C4
+enum Note {
+    //% blockIdentity=music.noteFrequency enumval=262
+    C = 262,
+    //% block=C#
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F = 349,
+    //% block=F#
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G = 392,
+    //% block=G#
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B = 494,
+    //% blockIdentity=music.noteFrequency enumval=131
+    C3 = 131,
+    //% block=C#3
+    //% blockIdentity=music.noteFrequency enumval=139
+    CSharp3 = 139,
+    //% blockIdentity=music.noteFrequency enumval=147
+    D3 = 147,
+    //% blockIdentity=music.noteFrequency enumval=156
+    Eb3 = 156,
+    //% blockIdentity=music.noteFrequency enumval=165
+    E3 = 165,
+    //% blockIdentity=music.noteFrequency enumval=175
+    F3 = 175,
+    //% block=F#3
+    //% blockIdentity=music.noteFrequency enumval=185
+    FSharp3 = 185,
+    //% blockIdentity=music.noteFrequency enumval=196
+    G3 = 196,
+    //% block=G#3
+    //% blockIdentity=music.noteFrequency enumval=208
+    GSharp3 = 208,
+    //% blockIdentity=music.noteFrequency enumval=220
+    A3 = 220,
+    //% blockIdentity=music.noteFrequency enumval=233
+    Bb3 = 233,
+    //% blockIdentity=music.noteFrequency enumval=247
+    B3 = 247,
+    //% blockIdentity=music.noteFrequency enumval=262
+    C4 = 262,
+    //% block=C#4
+    //% blockIdentity=music.noteFrequency enumval=277
+    CSharp4 = 277,
+    //% blockIdentity=music.noteFrequency enumval=294
+    D4 = 294,
+    //% blockIdentity=music.noteFrequency enumval=311
+    Eb4 = 311,
+    //% blockIdentity=music.noteFrequency enumval=330
+    E4 = 330,
+    //% blockIdentity=music.noteFrequency enumval=349
+    F4 = 349,
+    //% block=F#4
+    //% blockIdentity=music.noteFrequency enumval=370
+    FSharp4 = 370,
+    //% blockIdentity=music.noteFrequency enumval=392
+    G4 = 392,
+    //% block=G#4
+    //% blockIdentity=music.noteFrequency enumval=415
+    GSharp4 = 415,
+    //% blockIdentity=music.noteFrequency enumval=440
+    A4 = 440,
+    //% blockIdentity=music.noteFrequency enumval=466
+    Bb4 = 466,
+    //% blockIdentity=music.noteFrequency enumval=494
+    B4 = 494,
+    //% blockIdentity=music.noteFrequency enumval=523
+    C5 = 523,
+    //% block=C#5
+    //% blockIdentity=music.noteFrequency enumval=555
+    CSharp5 = 555,
+    //% blockIdentity=music.noteFrequency enumval=587
+    D5 = 587,
+    //% blockIdentity=music.noteFrequency enumval=622
+    Eb5 = 622,
+    //% blockIdentity=music.noteFrequency enumval=659
+    E5 = 659,
+    //% blockIdentity=music.noteFrequency enumval=698
+    F5 = 698,
+    //% block=F#5
+    //% blockIdentity=music.noteFrequency enumval=740
+    FSharp5 = 740,
+    //% blockIdentity=music.noteFrequency enumval=784
+    G5 = 784,
+    //% block=G#5
+    //% blockIdentity=music.noteFrequency enumval=831
+    GSharp5 = 831,
+    //% blockIdentity=music.noteFrequency enumval=880
+    A5 = 880,
+    //% blockIdentity=music.noteFrequency enumval=932
+    Bb5 = 932,
+    //% blockIdentity=music.noteFrequency enumval=988
+    B5 = 988,
+}
+
+//% weight=99 icon="\uf0e7" color=#1B80C4
 namespace CooCoo {
     /**
      * 设置电机
@@ -81,7 +203,7 @@ namespace CooCoo {
      * 停止单个电机
      */
     //% blockId="coocoo_stop" block="电机 停止 %direction"
-    //% weight=99
+    //% weight=98
     export function motorStop(direction: MotorDirection): void {
         if(direction == MotorDirection.left){
             pins.analogWritePin(AnalogPin.P15, 0);
@@ -97,7 +219,7 @@ namespace CooCoo {
     /**
      * 停止所有电机
      */
-    //% weight=10
+    //% weight=97
     //% blockId="coocoo_stopAll" block="停止所有电机"
     export function motorStopAll(): void {
         //右电机
@@ -109,75 +231,92 @@ namespace CooCoo {
     }
 
     /**
-     * 设置蜂鸣器
+     * 播放音调
      */
-    //% weight=10
-    //% blockId="coocoo_buzz" block="播放音符 %tone| 节拍 %beat"
-    export function buzz(tone: ToneHzTable, beat: BeatList): void {
-
-        let buf = pins.createBuffer(4);
-
-        buf[0] = 262&0xff;
-        buf[1] =  (262>>8)&0xff;
-        buf[2] = 400&0xff;
-        buf[3] =  (400>>8)&0xff;
-
-        // pins.analogWritePin(AnalogPin.P0, 1);
-        // pins.analogWritePin(AnalogPin.P0, 1);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-
-        // pins.analogWritePin(AnalogPin.P0, 1);
-
-        // pins.analogWritePin(AnalogPin.P0, 1);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-
-        // pins.analogWritePin(AnalogPin.P0, 1);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-        // pins.analogWritePin(AnalogPin.P0, 0);
-
-        // pins.analogWritePin(AnalogPin.P0, 1);
-
-        //pins.analogWritePin(AnalogPin.P0, 1023);
-        pins.digitalWritePin(DigitalPin.P0, 1);//试试这个
-        //pins.i2cWriteBuffer(0x02, buf);
+    //% weight=89
+    //% blockId="coocoo_tone" block="播放音符 %tone| 持续 %beat 节拍"
+    export function MyPlayTone(tone: ToneHzTable, beat: BeatList): void {
+        music.playTone(music.beat(tone),beat);
     }
+
+    /**
+     * 播放音乐
+     */
+    //% weight=88
+    //% blockId="coocoo_music" block="播放音乐 %song"
+    export function playMusic(song: MusicTable): void {
+        music.beginMelody(music.builtInMelody(song), MelodyOptions.Once);
+    }
+
+    /**
+     * 暂停播放
+     */
+    //% weight=87
+    //% blockId="coocoo_stop_tone" block="暂停播放 %beat 节拍"
+    export function playMusic(beat: BeatList): void {
+        music.rest(music.beat(beat));
+    }
+
+    /**
+     * Play a tone
+     * @param note pitch of the tone to play in Hertz (Hz), eg: Note.C
+     */
+    //% weight=86
+    //% blockId=test_music_play_note
+    //% block="play tone %note=test_device_note tone"
+    export function playTone(note: number, beat: BeatList) {
+        music.playTone(note,beat);
+    }
+
+    /**
+     * Get the frequency of a note.
+     * @param note the note name, eg: Note.C
+     */
+    //% weight=85
+    //% blockId=test_device_note block="%note"
+    //% shim=TD_ID
+    //% color="#ffffff" colorSecondary="#ffffff" colorTertiary="#1B80C4"
+    //% note.fieldEditor="note" note.defl="262"
+    //% note.fieldOptions.decompileLiterals=true
+    //% useEnumVal=1
+    export function noteFrequency(note: Note): number {
+        return note;
+    }
+
     
     //% weight=79
-    //% blockId=coocoo_patrol block="巡线啊呸 %patrol"
-    export function readPatrol(patrol:Patrol): number {
+    //% blockId=coocoo_patrol block="巡线传感器 %patrol"
+    export function readPatrol(patrol:Patrol): boolean {
 
         // let p1 = pins.digitalReadPin(DigitalPin.P13);
         // let p2 = pins.digitalReadPin(DigitalPin.P14);
 
         if(patrol == Patrol.white_white){
             if(pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 0){
-                return 1;
+                return true;
             }else{
-                return 0;
+                return false;
             }
         }else if(patrol == Patrol.white_black){
             if(pins.digitalReadPin(DigitalPin.P13) == 0 && pins.digitalReadPin(DigitalPin.P14) == 1){
-                return 1;
+                return true;
             }else{
-                return 0;
+                return false;
             }
         }else if(patrol == Patrol.black_white){
             if(pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 0){
-                return 1;
+                return true;
             }else{
-                return 0;
+                return false;
             }
         }else if(patrol == Patrol.black_black){
             if(pins.digitalReadPin(DigitalPin.P13) == 1 && pins.digitalReadPin(DigitalPin.P14) == 1){
-                return 1;
+                return true;
             }else{
-                return 0;
+                return false;
             }
         }else{
-            return -1;
+            return true;
         }
     }
 
