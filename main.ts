a1a9e5a67b21bf106b02f6845d758c615e0d8cdb
lixiang <214372150@qq.com>
2018-09-05 10:36:10 +0800

    test

--- main.ts
+++ main.ts
@@ -22,30 +22,6 @@ enum ToneHzTable {
     si = 494
 }
 
-enum BeatList {
-    //% block="1"
-    WHOLE = BeatFraction.Whole,
-    //% block="1/2"
-    HALF = BeatFraction.Half,
-    //% block="1/4"
-    QUARTER = BeatFraction.Quarter,
-    //% block="1/8"
-    EIGHTH = BeatFraction.Eighth,
-    //% block="1/16"
-    SIXTEEN = BeatFraction.SixTeenth,
-    //% block="2"
-    DOUBLE = BeatFraction.Double,
-    //% block="4"
-    BREVE = BeatFraction.Breve 
-}
-
-enum MusicTable {
-    //% block="生日快乐"
-    birthday = Melodies.Birthday,
-    //% block="婚礼进行曲"
-    wedding = Melodies.Wedding
-}
-
 enum Patrol{
     //% block="□□"
     white_white = 1,
@@ -56,118 +32,6 @@ enum Patrol{
     //% block="■■"
     black_black = 4
 }
-
-enum Note {
-    //% blockIdentity=music.noteFrequency enumval=262
-    C = 262,
-    //% block=C#
-    //% blockIdentity=music.noteFrequency enumval=277
-    CSharp = 277,
-    //% blockIdentity=music.noteFrequency enumval=294
-    D = 294,
-    //% blockIdentity=music.noteFrequency enumval=311
-    Eb = 311,
-    //% blockIdentity=music.noteFrequency enumval=330
-    E = 330,
-    //% blockIdentity=music.noteFrequency enumval=349
-    F = 349,
-    //% block=F#
-    //% blockIdentity=music.noteFrequency enumval=370
-    FSharp = 370,
-    //% blockIdentity=music.noteFrequency enumval=392
-    G = 392,
-    //% block=G#
-    //% blockIdentity=music.noteFrequency enumval=415
-    GSharp = 415,
-    //% blockIdentity=music.noteFrequency enumval=440
-    A = 440,
-    //% blockIdentity=music.noteFrequency enumval=466
-    Bb = 466,
-    //% blockIdentity=music.noteFrequency enumval=494
-    B = 494,
-    //% blockIdentity=music.noteFrequency enumval=131
-    C3 = 131,
-    //% block=C#3
-    //% blockIdentity=music.noteFrequency enumval=139
-    CSharp3 = 139,
-    //% blockIdentity=music.noteFrequency enumval=147
-    D3 = 147,
-    //% blockIdentity=music.noteFrequency enumval=156
-    Eb3 = 156,
-    //% blockIdentity=music.noteFrequency enumval=165
-    E3 = 165,
-    //% blockIdentity=music.noteFrequency enumval=175
-    F3 = 175,
-    //% block=F#3
-    //% blockIdentity=music.noteFrequency enumval=185
-    FSharp3 = 185,
-    //% blockIdentity=music.noteFrequency enumval=196
-    G3 = 196,
-    //% block=G#3
-    //% blockIdentity=music.noteFrequency enumval=208
-    GSharp3 = 208,
-    //% blockIdentity=music.noteFrequency enumval=220
-    A3 = 220,
-    //% blockIdentity=music.noteFrequency enumval=233
-    Bb3 = 233,
-    //% blockIdentity=music.noteFrequency enumval=247
-    B3 = 247,
-    //% blockIdentity=music.noteFrequency enumval=262
-    C4 = 262,
-    //% block=C#4
-    //% blockIdentity=music.noteFrequency enumval=277
-    CSharp4 = 277,
-    //% blockIdentity=music.noteFrequency enumval=294
-    D4 = 294,
-    //% blockIdentity=music.noteFrequency enumval=311
-    Eb4 = 311,
-    //% blockIdentity=music.noteFrequency enumval=330
-    E4 = 330,
-    //% blockIdentity=music.noteFrequency enumval=349
-    F4 = 349,
-    //% block=F#4
-    //% blockIdentity=music.noteFrequency enumval=370
-    FSharp4 = 370,
-    //% blockIdentity=music.noteFrequency enumval=392
-    G4 = 392,
-    //% block=G#4
-    //% blockIdentity=music.noteFrequency enumval=415
-    GSharp4 = 415,
-    //% blockIdentity=music.noteFrequency enumval=440
-    A4 = 440,
-    //% blockIdentity=music.noteFrequency enumval=466
-    Bb4 = 466,
-    //% blockIdentity=music.noteFrequency enumval=494
-    B4 = 494,
-    //% blockIdentity=music.noteFrequency enumval=523
-    C5 = 523,
-    //% block=C#5
-    //% blockIdentity=music.noteFrequency enumval=555
-    CSharp5 = 555,
-    //% blockIdentity=music.noteFrequency enumval=587
-    D5 = 587,
-    //% blockIdentity=music.noteFrequency enumval=622
-    Eb5 = 622,
-    //% blockIdentity=music.noteFrequency enumval=659
-    E5 = 659,
-    //% blockIdentity=music.noteFrequency enumval=698
-    F5 = 698,
-    //% block=F#5
-    //% blockIdentity=music.noteFrequency enumval=740
-    FSharp5 = 740,
-    //% blockIdentity=music.noteFrequency enumval=784
-    G5 = 784,
-    //% block=G#5
-    //% blockIdentity=music.noteFrequency enumval=831
-    GSharp5 = 831,
-    //% blockIdentity=music.noteFrequency enumval=880
-    A5 = 880,
-    //% blockIdentity=music.noteFrequency enumval=932
-    Bb5 = 932,
-    //% blockIdentity=music.noteFrequency enumval=988
-    B5 = 988,
-}
-
 //% weight=99 icon="\uf0e7" color=#1B80C4
 namespace CooCoo {
     /**
@@ -234,8 +98,8 @@ namespace CooCoo {
      * 播放音调
      */
     //% weight=89
-    //% blockId="coocoo_tone" block="播放音符 %tone| 持续 %beat 节拍"
-    export function MyPlayTone(tone: ToneHzTable, beat: BeatList): void {
+    //% blockId="coocoo_tone" block="播放音符"
+    export function MyPlayTone(): void {
         //music.playTone(music.beat(tone),beat);
         music.playTone(262, music.beat(BeatFraction.Whole))
     }
@@ -244,48 +108,12 @@ namespace CooCoo {
      * 播放音乐
      */
     //% weight=88
-    //% blockId="coocoo_music" block="播放音乐 %song"
-    export function MyPlayMusic(song: MusicTable): void {
+    //% blockId="coocoo_music" block="播放音乐"
+    export function MyPlayMusic(): void {
         // music.beginMelody(music.builtInMelody(song), MelodyOptions.Once);
         music.beginMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
     }
 
-    /**
-     * 暂停播放
-     */
-    //% weight=87
-    //% blockId="coocoo_stop_tone" block="暂停播放 %beat 节拍"
-    // export function stopTone(beat: BeatList): void {
-    //     music.rest(music.beat(beat));
-    // }
-
-    /**
-     * Play a tone
-     * @param note pitch of the tone to play in Hertz (Hz), eg: Note.C
-     */
-    //% weight=86
-    //% blockId=test_music_play_note
-    //% block="play tone %note=test_device_note tone"
-    // export function playToneTest(note: number, beat: BeatList) {
-    //     music.playTone(note,beat);
-    // }
-
-    /**
-     * Get the frequency of a note.
-     * @param note the note name, eg: Note.C
-     */
-    //% weight=85
-    //% blockId=test_device_note block="%note"
-    //% shim=TD_ID
-    //% color="#ffffff" colorSecondary="#ffffff" colorTertiary="#1B80C4"
-    //% note.fieldEditor="note" note.defl="262"
-    //% note.fieldOptions.decompileLiterals=true
-    //% useEnumVal=1
-    // export function noteFrequency(note: Note): number {
-    //     return note;
-    // }
-
-    
     //% weight=79
     //% blockId=coocoo_patrol block="巡线传感器 %patrol"
     export function readPatrol(patrol:Patrol): boolean {
