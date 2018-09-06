c4d1390dfe1ad414671f8deea8a1658a54619319
lixiang <214372150@qq.com>
2018-09-06 11:29:53 +0800

    test

--- main.ts
+++ main.ts
@@ -218,16 +218,18 @@ namespace CooCoo {
         }
     }
 
-    //% blockId=coocoo_sensor block="探测障碍物距离 %unit"
+    //% blockId=coocoo_sensor block="障碍物距离 %unit"
     //% weight=69
     export function sensorDistance(unit: PingUnit, maxCmDistance = 500): number {
         // send pulse
-        pins.setPull(DigitalPin.P2, PinPullMode.PullNone);
-        pins.digitalWritePin(DigitalPin.P2, 0);
+        pins.setPull(DigitalPin.P1, PinPullMode.PullNone);
+        pins.digitalWritePin(DigitalPin.P1, 0);
         control.waitMicros(2);
-        pins.digitalWritePin(DigitalPin.P2, 1);
+        pins.digitalWritePin(DigitalPin.P1, 1);
         control.waitMicros(10);
-        pins.digitalWritePin(DigitalPin.P2, 0);  
+        pins.digitalWritePin(DigitalPin.P1, 0);
+        
+        
 
         // read pulse
         let d = pins.pulseIn(DigitalPin.P2, PulseValue.High, maxCmDistance * 42);
