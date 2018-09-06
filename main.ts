77dca74ec7d30ee8145d7158b92c2f2bf95deeee
lixiang <214372150@qq.com>
2018-09-06 08:53:47 +0800

    test back

--- main.ts
+++ main.ts
@@ -129,14 +129,14 @@ namespace CooCoo {
         }else if(beatInfo == BeatList.quarter_beat){
             music.playTone(tone, music.beat(BeatFraction.Quarter));
 
-        }else if(beatInfo == BeatList.eighth_beat){
+        }else{
             music.playTone(tone, music.beat(BeatFraction.Eighth));
 
-        }else{
-            music.playTone(tone, music.beat(BeatFraction.SixTeenth));
-
         }
-        // else if(beatInfo == BeatList.double_beat){
+        // else if(beatInfo == BeatList.sixteen_beat){
+        //     music.playTone(tone, music.beat(BeatFraction.SixTeenth));
+
+        // }else if(beatInfo == BeatList.double_beat){
         //     music.playTone(tone, music.beat(BeatFraction.Double));
 
         // }else if(beatInfo == BeatList.breve_beat){
